2d45c6a9afa14c67169096dc9ffcaed6b8cce3c9
“Your <apurvdeshpande91@gmail.com>
2017-08-10 09:32:56 +0100

    Authorization complete

--- app.js
+++ app.js
@@ -28,13 +28,9 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname +"/public"));
 app.use(methodOverride("_method"));
-seedDB();
+//seedDB();
 
 
-app.use(function(req,res,next){
-  res.locals.currentUser = req.user;
-  next();
-});
 //Passport Configuration
 app.use(require("express-session")({
   secret: "Node JS",
@@ -47,6 +43,11 @@ app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req,res,next){
+  res.locals.currentUser = req.user;
+  next();
+});
 
 app.use(indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
